0ee32a5a69a5954e202acb97ad0ff54dd33811c0
Maximilian Raspe <60580904+knacker@users.noreply.github.com>
2021-05-07 10:17:37 +0200

    add project

---
+++ IR-System/Information-Retrieval-System/Information-Retrieval-System/Document.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Information_Retrieval_System
+{
+    class Document
+    {
+        private int id;
+        private String name;
+        private String content;
+
+
+        //filterlist for english words
+        private List<string> englishFilter = System.IO.File.ReadLines("/res/englishST.txt").ToList();
+
+        public Document(int id, String name, String content)
+        {
+            this.id = id;
+            this.name = name;
+            this.content = content;
+        }
+
+        public Tuple<int, String, String> getDocumentSpecs()
+        {
+            return Tuple.Create(id, name, content);
+        }
+
+        public void filter()
+        {
+
+        }
+
+        public void reduce()
+        {
+
+        }
+
+        public bool isRelevant(List<String> relevantDocs)
+        {
+            return true;
+        }
+
+        public void showContent()
+        {
+            Console.WriteLine(content);
+        }
+        public void showName()
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
---
+++ IR-System/Information-Retrieval-System/Information-Retrieval-System/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Information_Retrieval_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
