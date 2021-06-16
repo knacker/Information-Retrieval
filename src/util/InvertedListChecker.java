616b6ba8775b0ad48af5664e75f650bf23b8f140
Maximilian Raspe <60580904+knacker@users.noreply.github.com>
2021-06-16 09:25:24 +0200

    fix formatting

--- src/util/InvertedListChecker.java
+++ src/util/InvertedListChecker.java
@@ -38,28 +38,31 @@ public class InvertedListChecker {
                     }
                 }
             }
-        }
-        if (op.equals("&")) {
-            for (int j = 0; j < docsToCheck.size() - 1; j++) {
-                InvertedListObject obj1 = docsToCheck.get(j);
-                InvertedListObject obj2 = docsToCheck.get(j + 1);
 
-                for (Tuple<Integer, Integer> idCount1 : obj1.getIdCount()) {
-                    for (Tuple<Integer, Integer> idCount2 : obj2.getIdCount()) {
-                        if (idCount1.getValue1() == idCount2.getValue1()) {
-                            docIDs.add(idCount1.getValue1());
-                        } else {
-                            docIDs.remove(idCount1.getValue1());
+            if (op.equals("&")) {
+                for (int j = 0; j < docsToCheck.size() - 1; j++) {
+                    InvertedListObject obj1 = docsToCheck.get(j);
+                    InvertedListObject obj2 = docsToCheck.get(j + 1);
+
+                    for (Tuple<Integer, Integer> idCount1 : obj1.getIdCount()) {
+                        for (Tuple<Integer, Integer> idCount2 : obj2.getIdCount()) {
+                            if (idCount1.getValue1() == idCount2.getValue1()) {
+                                docIDs.add(idCount1.getValue1());
+                            } else {
+                                docIDs.remove(idCount1.getValue1());
+                            }
                         }
                     }
                 }
             }
-        }
-        if (op.equals("!")) {
+            if (op.equals("!")) {
+
+            }
+
 
         }
-
         return docIDs;
     }
 }
+
 
